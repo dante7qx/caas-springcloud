361ec0ae69b09ee8f6004ef82a24ab05205009e2
dante <sunchao.0129@163.com>
2018-08-20 23:44:06 +0800

    去除@EnableEurekaClient注解

--- caas-consumer/src/main/java/com/haihangyun/caas/ConsumerApplication.java
+++ caas-consumer/src/main/java/com/haihangyun/caas/ConsumerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +15,6 @@ import com.haihangyun.caas.client.AppFeignClient;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableFeignClients
 @RestController
 @RefreshScope
--- caas-provider/src/main/java/com/haihangyun/caas/ProviderApplication.java
+++ caas-provider/src/main/java/com/haihangyun/caas/ProviderApplication.java
@@ -4,12 +4,10 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
 @RestController
 public class ProviderApplication {
 	public static void main(String[] args) {
