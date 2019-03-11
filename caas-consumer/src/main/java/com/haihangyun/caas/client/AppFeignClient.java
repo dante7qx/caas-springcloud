51d96cd1d8bbc1a0f48bfbc804b744ed09bd0258
dante <sunchao.0129@163.com>
2019-03-11 15:11:43 +0800

    xiugai

--- caas-consumer/src/main/java/com/haihangyun/caas/client/AppFeignClient.java
+++ caas-consumer/src/main/java/com/haihangyun/caas/client/AppFeignClient.java
@@ -2,13 +2,17 @@ package com.haihangyun.caas.client;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.haihangyun.caas.vo.TechnologVO;
 
-@FeignClient(name = "caas-provider", fallback = AppFeignClientFallback.class)
+@Profile("prd")
+@Qualifier("appFeignClient")
+@FeignClient(name="${consumer.provider}", url = "http://${consumer.provider_path}", fallback = AppFeignClientFallback.class)
 public interface AppFeignClient {
 
 	@RequestMapping(value = "/msg", method = RequestMethod.GET)
